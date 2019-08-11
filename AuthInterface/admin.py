61cd2984acde12fa85cafb2de58b750b159b0eea
Muhammad Kamran <mkrnaqeebi@gmail.com>
2019-08-12 05:32:45 +0800

    header title added

--- AuthInterface/admin.py
+++ AuthInterface/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import User
+from django.utils.translation import ugettext_lazy as _
 
+admin.site.site_header = _('Voo Shop')
+admin.site.site_title = _('Voo Shop')
 # Register your models here.
 
 admin.site.register(User)
