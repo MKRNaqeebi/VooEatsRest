0ce66a9a50c3bfa9076d0e00d0378e6c07997137
Muhammad Kamran <mkrnaqeebi@gmail.com>
2019-08-13 01:43:32 +0800

    remove create

--- ShopInterface/views.py
+++ ShopInterface/views.py
@@ -4,11 +4,11 @@ from .serializers import ShopSerializer
 
 # Create your views here.
 
-class ShopViewSet(generics.ListCreateAPIView):
+class ShopViewSet(generics.ListAPIView):
     """ API endpoint that sorts the circuits by given parameter """
     permission_classes = (permissions.AllowAny,)
     serializer_class = ShopSerializer
     queryset = Shop.objects.all()
     
-    def perform_create(self, serializer):
-        instance = serializer.save(owner = self.request.user)
+    # def perform_create(self, serializer):
+    #     instance = serializer.save(owner = self.request.user)
